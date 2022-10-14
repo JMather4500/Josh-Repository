1e7e59261f333facf623bc12031bdec1a028b440
JoshuaMather4500 <joshuaiain@gmail.com>
2022-10-14 13:39:52 +0100

    added doSeventimesTable moved private static void outside of main and added an output

--- src/Week2/Week02.java
+++ src/Week2/Week02.java
@@ -1,7 +1,6 @@
 package Week2;
-import helpers.InputReader;
 
-import java.util.helpers;
+import helpers.InputReader;
 public class Week02
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
@@ -18,18 +17,22 @@ public class Week02
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
 
-        Public static void doSevenTimesTable()
-        {
+        doSevenTimesTable();
 
-            int multiplicand = 7;
-            int product;        //this will be the sum of the mutliplicand and multiplier
-            int multiplier;     //This will be inputted by the user
 
-            multiplier = InputReader.getInt("Please enter a value to multiply by 7 ");
-           product = multiplicand * multiplier;
-
-        } //sevenTimesTable ends
     }
+    private static void doSevenTimesTable()
+    {
+
+        int multiplicand = 7;
+        int product;        //this will be the sum of the mutliplicand and multiplier
+        int multiplier;     //This will be inputted by the user
+
+        multiplier = InputReader.getInt("Please enter a value to multiply by 7 ");
+        product = multiplicand * multiplier;
+        System.out.println(product);
+
+    } //doSevenTimesTable ends
 
 
 }
