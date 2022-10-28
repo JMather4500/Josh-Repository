26c80da58ae4fa19d9816360412e8cdaf0c41b7c
JoshuaMather4500 <joshuaiain@gmail.com>
2022-10-28 16:26:32 +0100

    added doSeventimesTable moved private static void outside of main and added an output added the multiplyBySeven method
    added an array list and made it print
    Added beggining of else if statement
    added numbers to songs list
    turned Array list into a Hash map
    Changed hash map back to array list, also started trying to make it possible to add a new song to the code

--- src/Assessment/Assessment.java
+++ src/Assessment/Assessment.java
@@ -1,24 +1,11 @@
 package Assessment;
 
 import helpers.InputReader;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Assessment {
-    public static final String CONSOLE_CLEAR = "\033[H\033[2J";
 
     public static void main(String[] args) {
-        System.out.println(CONSOLE_CLEAR);
-
-        System.out.println("==================================");
-        System.out.println("CO452 Programming Concepts 2022/23");
-        System.out.println("==================================");
-        System.out.println();
-        System.out.println("  Week 2 Java Selection & Iteration ");
-        System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
-        System.out.println();
 
 
         songList();
@@ -32,40 +19,56 @@ public class Assessment {
 
         int songListCommand;
 
-        HashMap<String, Integer> songs = new HashMap<String, Integer>();
-            songs.put("Bad Medicine",2264);
-            songs.put("Bohemian Rhapsody", 5568);
-            songs.put("Passage To Bangkok", 2589) ;
-            songs.put("Heat of the Moment", 1598);
-            songs.put("The Spirit Of Radio", 6894);
-            songs.put("Freewill",8498);
-            songs.put("Uprising",2254);
-        System.out.println(songs);
+            ArrayList<Song> songs = new ArrayList<Song>();  //adding default songs to list
 
-        System.out.println("please enter the number associated with a command");
-        System.out.println("1: Add song");
-        System.out.println("2: Remove song");
-        System.out.println("3.show all songs");
-        System.out.println("4: show songs with a certain number of plays or more ");
+                Song music = new Song("Bad Medicine", "artist", 1234 ); //new music object for song array list
+                    songs.add(music); //adding to list
+                new Song("Bohemian Rhapsody", "Queen", 50000);
+                    songs.add(music);
+                new Song("Passage To Bangkok", "Rush", 3215) ;
+                    songs.add(music);
+                new Song("Heat of the Moment", "NSP", 5674);
+                    songs.add(music);
+                new Song("The Spirit Of Radio", "Rush", 24685);
+                    songs.add(music);
+                new Song("Freewill", "Rush", 84544);
+                    songs.add(music);
+               new Song("Uprising", "Muse", 45746);
+                    songs.add(music);
+
+
+                System.out.println("please enter the number associated with a command");
+                System.out.println("1: Add song");
+                System.out.println("2: Remove song");
+                System.out.println("3.show all songs");
+                System.out.println("4: show songs with a certain number of plays or more ");
 
         songListCommand = InputReader.getInt("What do you want to do? ");
 
-            if (songListCommand == 1)   //This will prompt the user to enter a new song
-            {
-                songs.put(InputReader.getString("Please enter the name of the song you want to add "));
-            }
-            else if (songListCommand == 2) // This will prompt the user to remove a song
-            {
-                songs.remove(InputReader.getInt("Please enter the name of the song you wish to remove "));
-            }
-            else if (songListCommand == 3)  // This will display the songs as they appear in the array
-            {
-            System.out.println(songs);
-            }
-            else if (songListCommand == 4) //This will display songs over a given number of plays
-            {
-            System.out.println("test");
-            }//end of if statement
-    }
 
+        //make a while loop
+        if (songListCommand == 1)   //This will prompt the user to enter a new song
+        {
+           // songs.add(InputReader.getString("Please enter the name of the song you want to add "));
+
+        }
+        else if (songListCommand == 2) // This will prompt the user to remove a song
+        {
+            songs.remove(InputReader.getInt("Please enter the name of the song you wish to remove "));
+        }
+        else if (songListCommand == 3)  // This will display the songs as they appear in the array
+            System.out.println(songs);
+        else if (songListCommand == 4) //This will display songs over a given number of plays
+        {
+            System.out.println("test");
+        }//end of if statement
     }
+    private Song[] newSong()
+        {
+            Song[] addNewSong = new Song[3];
+
+            addNewSong[0] = InputReader.getString("Please enter the name of the song");
+
+        }
+
+}
---
+++ src/Assessment/Song.java
@@ -0,0 +1,22 @@
+package Assessment;
+
+public class Song {
+
+      // private String id;
+
+        private String title;
+
+        private String artistName;
+
+        private int playCount;
+
+        public Song(String title, String name, int count)
+        {
+            this.title = title;
+            this.artistName = name;
+            this.playCount = count;
+
+        }
+
+}
+
